31e258bb65c359dc8088706f8295bf6fe20381cb
Серж <Zotbotzot@yandex.ru>
2021-04-28 18:51:34 +0300

    Д/З

--- src/main/java/homework/lesson2/task2/Chess_board.java
+++ src/main/java/homework/lesson2/task2/Chess_board.java
@@ -1,4 +1,38 @@
 package homework.lesson2.task2;
 
 public class Chess_board {
+
+    public static void main(String[] args) {
+        int[][] chessBoard = new int[8][8];
+
+        for (int i = 0; i < chessBoard.length; i++){
+            for (int j = 0; j <chessBoard.length; j++)
+                chessBoard[i][j] = (i + j) % 2;
+        }
+        System.out.println("Вывод массива через For");
+        for (int i = 0; i < chessBoard.length; i++){
+            for (int j = 0; j < chessBoard.length; j++)
+                System.out.print(chessBoard[i][j]);
+            System.out.println("");
+        }
+        System.out.println("Вывод массива через ForEach");
+        for (int[] i : chessBoard){
+            for (int j : i)
+                System.out.print(j);
+            System.out.println("");
+
+        }
+        System.out.println("Вывод массива через While");
+        int i = 0;
+        int j = 0;
+        while (i < chessBoard.length){
+            while (j < chessBoard.length) {
+                System.out.print(chessBoard[i][j]);
+                j++;
+            }
+            i++;
+            j = 0;
+            System.out.println("");
+        }
+    }
 }
---
+++ src/main/java/homework/lesson2/task3/Phone.java
@@ -0,0 +1,59 @@
+package homework.lesson2.task3;
+
+public class Phone {
+    public static void main(String[] args) {
+
+        Phone phone = new Phone();
+
+        System.out.println(phone.matches("+7(910)423-73-12"));
+        System.out.println(phone.matches("7(910)423-73-12"));
+        System.out.println(phone.matches("89215310934"));
+        System.out.println(phone.matches("+5(911)310-12-74"));
+        System.out.println(phone.matches("+7(9fg)125-42-99"));
+        System.out.println(phone.matches("+7(122)2342343"));
+        System.out.println();
+        System.out.println(phone.cycles("+7(910)423-73-12"));
+        System.out.println(phone.cycles("7(910)423-73-12"));
+        System.out.println(phone.cycles("89215310934"));
+        System.out.println(phone.cycles("+5(911)310-12-74"));
+        System.out.println(phone.cycles("+7(9fg)125-42-99"));
+        System.out.println(phone.cycles("+7(122)2342343"));
+
+
+    }
+
+    boolean matches(String phone){
+        return phone.matches("^\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$");
+    }
+
+    boolean cycles(String phone){
+
+        if (phone == null || phone.length() != 16){
+            return false;
+        }
+        if (!phone.startsWith("+7(")){
+            return false;
+        }
+        if (phone.charAt(6) != ')'){
+            return false;
+        }
+        if (phone.charAt(10) != '-'){
+            return false;
+        }
+        if (phone.charAt(13) != '-'){
+            return false;
+        }
+
+        int[] element = {3, 4, 5, 7, 8, 9, 11, 12, 14, 15};
+
+        String namber = "0123456789";
+        
+        for (int index : element){
+            String ind = String.valueOf(phone.charAt(index));
+            if (!namber.contains(ind)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
